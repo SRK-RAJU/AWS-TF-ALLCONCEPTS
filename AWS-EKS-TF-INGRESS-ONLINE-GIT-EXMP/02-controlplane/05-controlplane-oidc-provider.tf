f0c628ee553e8655587ffdfd5a89b13799c7ac50
91994 <suddala.rajukumar123@gmail.com>
2022-11-11 12:06:27 +0530

    updating

--- AWS-EKS-TF-INGRESS-ONLINE-GIT-EXMP/02-controlplane/05-controlplane-oidc-provider.tf
+++ AWS-EKS-TF-INGRESS-ONLINE-GIT-EXMP/02-controlplane/05-controlplane-oidc-provider.tf
@@ -4,7 +4,7 @@
  */
 
 data "external" "thumbprint" {
-  program = ["./05-controlplane-oidc-provider-thumbprint.sh", data.aws_region.current.name]
+  program = ["./06-controlplane-oidc-provider-thumbprint.sh", data.aws_region.current.name]
 }
 resource "aws_iam_openid_connect_provider" "EksControlPlane" {
   client_id_list  = ["sts.amazonaws.com"]
